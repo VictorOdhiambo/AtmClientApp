7d4cb731ae8f91e471c2ab0b9561aa421e8a9c63
vomondi <vomondi@fintech-group.com>
2024-01-22 10:03:05 +0300

    Modified the base url from localhost to cloud based

--- src/data/BaseUrl.js
+++ src/data/BaseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://odhizvic-001-site1.itempurl.com"
+export const BASE_URL = "https://odhizvic-001-site1.itempurl.com"
 //export const BASE_URL = "https://localhost:7276"
